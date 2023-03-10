cbce153040f6f4f1f4c60eced02a221ec622cc52
Dahian Nuñez <dayannunez.ba@gmail.com>
2023-03-10 00:56:58 -0600

    Validar campos

--- capaPresentacionValidadorPerfiles/App_Start/BundleConfig.cs
+++ capaPresentacionValidadorPerfiles/App_Start/BundleConfig.cs
@@ -18,7 +18,9 @@ namespace capaPresentacionValidadorPerfiles
                         "~/Scripts/DataTables/dataTables.responsive.js",
                          "~/Scripts/loadingoverlay/loadingoverlay.min.js",
                          "~/Scripts/Emparejamiento.js",
-                         "~/Scripts/sweetalert.min.js"
+                         "~/Scripts/sweetalert.min.js",
+                         "~/Scripts/jquery.validate.js",
+                         "~/Scripts/jquery-ui.js"
 
                         ));
 
@@ -32,7 +34,8 @@ namespace capaPresentacionValidadorPerfiles
                 "~/Content/DataTables/css/jquery.dataTables.css",
                 "~/Content/DataTables/css/responsive.dataTables.css",
                 "~/Content/Emparejamiento.css",
-                "~/Content/sweetalert.css"
+                "~/Content/sweetalert.css",
+                "~/Content/jquery-ui.css"
                 ));
         }
     }
