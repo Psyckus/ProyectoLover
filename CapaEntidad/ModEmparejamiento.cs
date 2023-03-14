1f69e5e2ecc7a5abe483afe9793928c59698629d
Kevincg16 <kevinj.calvo12@gmail.com>
2023-03-13 18:02:27 -0600

    Mod emparejamiento v2

---
+++ CapaEntidad/ModEmparejamiento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class ModEmparejamiento
+    {
+
+        public int idModConf { get; set; }
+        public int CriterioInteres { get; set; }
+        public bool TestCriterio { get; set; }
+        public bool EdadCriterio { get; set; }
+    }
+}
---
+++ CapaNegocios/CN_ModEmparejamiento.cs
@@ -0,0 +1,41 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    public class CN_ModEmparejamiento
+    {
+        private CD_ModEmparejamieto objCapaDato = new CD_ModEmparejamieto();
+
+        public List<ModEmparejamiento> ListaModulo()
+        {
+            return objCapaDato.ListaModulo();
+        }
+
+        public bool editar(ModEmparejamiento obj, out string mensaje)
+        {
+            mensaje = string.Empty;
+
+            if (obj.CriterioInteres == 0)
+            {
+                mensaje = "Debe ingresar wl criterio de interes";
+            }
+
+
+            if (string.IsNullOrEmpty(mensaje))
+            {
+                return objCapaDato.editarModulo(obj, out mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
